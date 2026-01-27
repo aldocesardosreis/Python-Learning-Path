c627e0a3594ff73de87a2a7ca2fa3fb899aea896
Aldo_Cesar <aldocesarreis.midia@gmail.com>
2026-01-26 22:11:24 -0600

    Update ex_017.py

--- Exercices/1-Course/ex_017.py
+++ Exercices/1-Course/ex_017.py
@@ -1,1 +1,9 @@
-#Exercise 017 - 
+#Exercise 017 - Write a program that reads the lengths of the opposite and adjacent legs of a right triangle. Calculate and display the length of the hypotenuse.
+
+import math
+
+a = float(input('Enter the opposite leg: '))
+b = float(input('Enter the adjacent leg: '))
+c = math.sqrt((a**2)+(b**2))
+
+print('The hypotenuse is {:.2f}'.format(c))

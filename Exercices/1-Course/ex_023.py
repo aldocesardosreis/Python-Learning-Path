089f7fe77023d241208ca8eba9d6ae65ba4f54bf
Aldo_Cesar <aldocesarreis.midia@gmail.com>
2026-02-04 16:40:19 -0600

    Correct ex_023

--- Exercices/1-Course/ex_023.py
+++ Exercices/1-Course/ex_023.py
@@ -1,10 +1,6 @@
 #Exercise 023 - Write a program that reads a number from 0 to 9999 and diaplays each of its digits separately on the screen.
 
 n = int(input("Enter a number from 0 to 9999: "))
-n1 = ("{:04}".format(n))
-numbers = str(n1.split())
-print("{}\n units: \n tens: \n hundreads: \n thousands: ".format(n1))
-print(numbers[3]) 
-print(numbers[2]) 
-print(numbers[1]) 
-print(numbers[0])
+n1 = str("{:04}".format(n))
+print("Number {}\n units: {}\n tens: {}\n hundreads: {}\n thousands: {}".format(n1, n1[3], n1[2], n1[1], n1[0]))
+    

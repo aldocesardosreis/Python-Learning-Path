d93294758e30a185b355d65714e519721572d145
Aldo_Cesar <aldocesarreis.midia@gmail.com>
2026-02-05 00:07:01 -0600

    ex_024

--- Exercices/1-Course/ex_023.py
+++ Exercices/1-Course/ex_023.py
@@ -3,4 +3,3 @@
 n = int(input("Enter a number from 0 to 9999: "))
 n1 = str("{:04}".format(n))
 print("Number {}\n units: {}\n tens: {}\n hundreads: {}\n thousands: {}".format(n1, n1[3], n1[2], n1[1], n1[0]))
-    
---
+++ Exercices/1-Course/ex_024.py
@@ -0,0 +1,3 @@
+#Exercise 024 - Create a program that reads the name of a city and tells whether or not it starts with the name "SANTO"
+
+city = input("Type a city's name: ")
